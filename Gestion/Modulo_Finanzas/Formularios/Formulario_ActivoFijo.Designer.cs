80e6ec4280cadbd875f590275918fe29074f3642
Juan Mejia <jumej502@gmail.com>
2016-11-09 18:20:51 -0600

    Activo Fijo
    Diferentes Depreciaciones

--- Gestion/Modulo_Finanzas/Formularios/Formulario_ActivoFijo.Designer.cs
+++ Gestion/Modulo_Finanzas/Formularios/Formulario_ActivoFijo.Designer.cs
@@ -29,10 +29,7 @@
         private void InitializeComponent()
         {
             this.btnRegistrarActivo = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
             this.txtDepreciacion = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -40,66 +37,43 @@
             this.txtTiempoDepreciacion = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // btnRegistrarActivo
             // 
-            this.btnRegistrarActivo.Location = new System.Drawing.Point(486, 269);
+            this.btnRegistrarActivo.Location = new System.Drawing.Point(438, 298);
             this.btnRegistrarActivo.Name = "btnRegistrarActivo";
-            this.btnRegistrarActivo.Size = new System.Drawing.Size(174, 96);
+            this.btnRegistrarActivo.Size = new System.Drawing.Size(153, 32);
             this.btnRegistrarActivo.TabIndex = 25;
             this.btnRegistrarActivo.Text = "REGISTRAR ACTIVO";
             this.btnRegistrarActivo.UseVisualStyleBackColor = true;
             this.btnRegistrarActivo.Click += new System.EventHandler(this.btnRegistrarActivo_Click);
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(413, 175);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(116, 13);
-            this.label6.TabIndex = 24;
-            this.label6.Text = "% DE DEPRESIACION";
-            // 
             // txtDepreciacion
             // 
-            this.txtDepreciacion.Location = new System.Drawing.Point(582, 172);
+            this.txtDepreciacion.Enabled = false;
+            this.txtDepreciacion.Location = new System.Drawing.Point(377, 78);
             this.txtDepreciacion.Name = "txtDepreciacion";
-            this.txtDepreciacion.Size = new System.Drawing.Size(78, 20);
+            this.txtDepreciacion.Size = new System.Drawing.Size(62, 20);
             this.txtDepreciacion.TabIndex = 23;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(54, 175);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(178, 13);
-            this.label5.TabIndex = 22;
-            this.label5.Text = "FECHA DE INICIO DEPRESIACION";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(244, 168);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(157, 20);
-            this.textBox5.TabIndex = 21;
+            this.txtDepreciacion.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(52, 119);
+            this.label4.Location = new System.Drawing.Point(442, 61);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(149, 13);
             this.label4.TabIndex = 20;
-            this.label4.Text = "TIEMPO DE DEPRESIACION";
+            this.label4.Text = "TIEMPO DE DEPRECIACION";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(115, 66);
+            this.label3.Location = new System.Drawing.Point(375, 194);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(47, 13);
             this.label3.TabIndex = 19;
@@ -108,16 +82,16 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(54, 37);
+            this.label2.Location = new System.Drawing.Point(41, 124);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.Size = new System.Drawing.Size(80, 13);
             this.label2.TabIndex = 18;
-            this.label2.Text = "NOMBRE";
+            this.label2.Text = "DESCRIPCION";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(54, 145);
+            this.label1.Location = new System.Drawing.Point(44, 191);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(109, 13);
             this.label1.TabIndex = 17;
@@ -125,36 +99,31 @@
             // 
             // txtTiempoDepreciacion
             // 
-            this.txtTiempoDepreciacion.Location = new System.Drawing.Point(244, 112);
+            this.txtTiempoDepreciacion.Enabled = false;
+            this.txtTiempoDepreciacion.Location = new System.Drawing.Point(445, 78);
             this.txtTiempoDepreciacion.Name = "txtTiempoDepreciacion";
-            this.txtTiempoDepreciacion.Size = new System.Drawing.Size(157, 20);
+            this.txtTiempoDepreciacion.Size = new System.Drawing.Size(146, 20);
             this.txtTiempoDepreciacion.TabIndex = 16;
+            this.txtTiempoDepreciacion.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(174, 63);
+            this.textBox3.Location = new System.Drawing.Point(434, 191);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(157, 20);
             this.textBox3.TabIndex = 15;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(114, 30);
+            this.textBox2.Location = new System.Drawing.Point(44, 145);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(546, 20);
             this.textBox2.TabIndex = 14;
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(244, 138);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(157, 20);
-            this.textBox1.TabIndex = 13;
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(411, 65);
+            this.label7.Location = new System.Drawing.Point(45, 61);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(51, 13);
             this.label7.TabIndex = 26;
@@ -168,24 +137,30 @@
             "MOBILIARIO Y EQUIPO",
             "VEHICULOS",
             "EQUIPO DE COMPUTO"});
-            this.comboBox1.Location = new System.Drawing.Point(468, 62);
+            this.comboBox1.Location = new System.Drawing.Point(47, 77);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(192, 21);
+            this.comboBox1.Size = new System.Drawing.Size(323, 21);
             this.comboBox1.TabIndex = 27;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(159, 191);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(109, 20);
+            this.dateTimePicker1.TabIndex = 28;
             // 
             // Formulario_ActivoFijo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(672, 377);
+            this.ClientSize = new System.Drawing.Size(650, 375);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.btnRegistrarActivo);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.txtDepreciacion);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -193,7 +168,6 @@
             this.Controls.Add(this.txtTiempoDepreciacion);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Name = "Formulario_ActivoFijo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Formulario_ActivoFijo";
@@ -206,10 +180,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnRegistrarActivo;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtDepreciacion;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
@@ -217,8 +188,8 @@
         private System.Windows.Forms.TextBox txtTiempoDepreciacion;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
