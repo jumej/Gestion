2a71cc52ac12f526a0dbef8908785aadaeec0c09
SixPack-AA <EstuardoA25@gmail.com>
2016-11-09 22:32:44 -0600

    Añadido el modulo de recursos humanos

---
+++ Gestion/Modulo_RRHH/Formularios/Formulario_Empleados.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion.Modulo_RRHH.Formularios
+{
+    public partial class Formulario_Empleados : Form
+    {
+        public Formulario_Empleados()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Gestion/Modulo_RRHH/Formularios/Formulario_Reclutar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gestion.Clases_Generales;
+using MySql.Data.MySqlClient;
+
+namespace Gestion.Modulo_RRHH.Formularios
+{
+    public partial class Formulario_Reclutar : Form
+    {
+        public Formulario_Reclutar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conexion = Clase_ConeccionDB.obtenerConeccion();
+            MySqlCommand cmd = conexion.CreateCommand();
+            String nombre = inputFirstname.Text;
+            String apellido = inputLastname.Text;
+            int telefono = Convert.ToInt32(inputPhone.Text);
+            String email = inputEmail.Text;
+            String direccion = inputAddress.Text;
+            cmd.CommandText = ("INSERT INTO curriculum (Nombre, Apellido, Telefono, Correo_Electronico, Direccion) VALUES('" + nombre + "','" + apellido + "','" + telefono + "','" + email + "','" + direccion + "')");
+            cmd.ExecuteNonQuery();
+            inputFirstname.Text = "";
+            inputLastname.Text = "";
+            inputPhone.Text = "";
+            inputAddress.Text = "";
+            inputEmail.Text = "";
+            MessageBox.Show("Persona ingresada correctamente");
+        }
+    }
+}
