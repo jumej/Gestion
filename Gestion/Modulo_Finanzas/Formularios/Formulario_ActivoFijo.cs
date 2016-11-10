2122e390f1c755ca03250e2eae9574f3cb6646a4
Juan Mejia <jumej502@gmail.com>
2016-11-09 21:29:19 -0600

    Finanzas
    Agregue Activo Fijo, Balance, Mayor

---
+++ Gestion/Modulo_Finanzas/Formularios/Formulario_ActivoFijo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion.Modulo_Finanzas.Formularios
+{
+    public partial class Formulario_ActivoFijo : Form
+    {
+        public Formulario_ActivoFijo()
+        {
+            InitializeComponent();
+        }
+
+        private void Formulario_ActivoFijo_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
