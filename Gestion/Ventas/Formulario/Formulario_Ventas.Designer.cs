6d5eef4f09de81106e6fdb5ddeafbb90643b8f20
Pablo <ppablo_30@hotmail.com>
2016-11-10 01:26:26 -0600

    Cerotes

--- Gestion/Ventas/Formulario/Formulario_Ventas.Designer.cs
+++ Gestion/Ventas/Formulario/Formulario_Ventas.Designer.cs
@@ -61,111 +61,124 @@
             // m1
             // 
             this.m1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.m1.Location = new System.Drawing.Point(17, 10);
+            this.m1.Location = new System.Drawing.Point(26, 16);
+            this.m1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.m1.Name = "m1";
-            this.m1.Size = new System.Drawing.Size(110, 71);
+            this.m1.Size = new System.Drawing.Size(205, 164);
             this.m1.TabIndex = 0;
             this.m1.Text = "Mesa1";
             this.m1.UseVisualStyleBackColor = false;
             // 
             // m2
             // 
-            this.m2.Location = new System.Drawing.Point(147, 10);
+            this.m2.Location = new System.Drawing.Point(237, 16);
+            this.m2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.m2.Name = "m2";
-            this.m2.Size = new System.Drawing.Size(97, 71);
+            this.m2.Size = new System.Drawing.Size(205, 164);
             this.m2.TabIndex = 1;
             this.m2.Text = "Mesa2";
             this.m2.UseVisualStyleBackColor = true;
             // 
             // m3
             // 
-            this.m3.Location = new System.Drawing.Point(268, 10);
+            this.m3.Location = new System.Drawing.Point(450, 16);
+            this.m3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.m3.Name = "m3";
-            this.m3.Size = new System.Drawing.Size(105, 71);
+            this.m3.Size = new System.Drawing.Size(203, 164);
             this.m3.TabIndex = 2;
             this.m3.Text = "Mesa3";
             this.m3.UseVisualStyleBackColor = true;
             // 
             // m4
             // 
-            this.m4.Location = new System.Drawing.Point(17, 108);
+            this.m4.Location = new System.Drawing.Point(661, 16);
+            this.m4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.m4.Name = "m4";
-            this.m4.Size = new System.Drawing.Size(110, 71);
+            this.m4.Size = new System.Drawing.Size(203, 164);
             this.m4.TabIndex = 3;
             this.m4.Text = "Mesa4";
             this.m4.UseVisualStyleBackColor = true;
             // 
             // m5
             // 
-            this.m5.Location = new System.Drawing.Point(147, 105);
+            this.m5.Location = new System.Drawing.Point(26, 190);
+            this.m5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.m5.Name = "m5";
-            this.m5.Size = new System.Drawing.Size(97, 71);
+            this.m5.Size = new System.Drawing.Size(203, 164);
             this.m5.TabIndex = 4;
             this.m5.Text = "Mesa5";
             this.m5.UseVisualStyleBackColor = true;
             // 
             // m6
             // 
-            this.m6.Location = new System.Drawing.Point(268, 105);
+            this.m6.Location = new System.Drawing.Point(239, 190);
+            this.m6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.m6.Name = "m6";
-            this.m6.Size = new System.Drawing.Size(105, 71);
+            this.m6.Size = new System.Drawing.Size(203, 164);
             this.m6.TabIndex = 5;
             this.m6.Text = "Mesa6";
             this.m6.UseVisualStyleBackColor = true;
             // 
             // m7
             // 
-            this.m7.Location = new System.Drawing.Point(17, 202);
+            this.m7.Location = new System.Drawing.Point(450, 190);
+            this.m7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.m7.Name = "m7";
-            this.m7.Size = new System.Drawing.Size(110, 71);
+            this.m7.Size = new System.Drawing.Size(203, 164);
             this.m7.TabIndex = 6;
             this.m7.Text = "Mesa7";
             this.m7.UseVisualStyleBackColor = true;
             // 
             // m8
             // 
-            this.m8.Location = new System.Drawing.Point(147, 199);
+            this.m8.Location = new System.Drawing.Point(661, 190);
+            this.m8.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.m8.Name = "m8";
-            this.m8.Size = new System.Drawing.Size(97, 71);
+            this.m8.Size = new System.Drawing.Size(203, 164);
             this.m8.TabIndex = 7;
             this.m8.Text = "Mesa8";
             this.m8.UseVisualStyleBackColor = true;
             // 
             // m9
             // 
-            this.m9.Location = new System.Drawing.Point(268, 199);
+            this.m9.Location = new System.Drawing.Point(26, 364);
+            this.m9.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.m9.Name = "m9";
-            this.m9.Size = new System.Drawing.Size(105, 71);
+            this.m9.Size = new System.Drawing.Size(203, 164);
             this.m9.TabIndex = 8;
             this.m9.Text = "Mesa9";
             this.m9.UseVisualStyleBackColor = true;
             // 
             // m10
             // 
-            this.m10.Location = new System.Drawing.Point(17, 292);
+            this.m10.Location = new System.Drawing.Point(237, 364);
+            this.m10.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.m10.Name = "m10";
-            this.m10.Size = new System.Drawing.Size(110, 71);
+            this.m10.Size = new System.Drawing.Size(205, 164);
             this.m10.TabIndex = 9;
             this.m10.Text = "Mesa10";
             this.m10.UseVisualStyleBackColor = true;
             // 
             // m11
             // 
-            this.m11.Location = new System.Drawing.Point(147, 292);
+            this.m11.Location = new System.Drawing.Point(450, 364);
+            this.m11.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.m11.Name = "m11";
-            this.m11.Size = new System.Drawing.Size(97, 71);
+            this.m11.Size = new System.Drawing.Size(205, 164);
             this.m11.TabIndex = 10;
             this.m11.Text = "Mesa11";
             this.m11.UseVisualStyleBackColor = true;
             // 
             // m12
             // 
-            this.m12.Location = new System.Drawing.Point(268, 289);
+            this.m12.Location = new System.Drawing.Point(663, 364);
+            this.m12.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.m12.Name = "m12";
-            this.m12.Size = new System.Drawing.Size(105, 71);
+            this.m12.Size = new System.Drawing.Size(205, 164);
             this.m12.TabIndex = 11;
             this.m12.Text = "Mesa12";
             this.m12.UseVisualStyleBackColor = true;
+            this.m12.Click += new System.EventHandler(this.m12_Click);
             // 
             // panel3
             // 
@@ -182,16 +195,18 @@
             this.panel3.Controls.Add(this.m3);
             this.panel3.Controls.Add(this.m2);
             this.panel3.Controls.Add(this.m1);
-            this.panel3.Location = new System.Drawing.Point(96, 28);
+            this.panel3.Location = new System.Drawing.Point(144, 45);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(390, 379);
+            this.panel3.Size = new System.Drawing.Size(909, 567);
             this.panel3.TabIndex = 24;
             // 
             // btncolor1
             // 
-            this.btncolor1.Location = new System.Drawing.Point(386, 413);
+            this.btncolor1.Location = new System.Drawing.Point(376, 622);
+            this.btncolor1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btncolor1.Name = "btncolor1";
-            this.btncolor1.Size = new System.Drawing.Size(40, 23);
+            this.btncolor1.Size = new System.Drawing.Size(60, 37);
             this.btncolor1.TabIndex = 25;
             this.btncolor1.UseVisualStyleBackColor = true;
             // 
@@ -199,7 +214,8 @@
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(492, 416);
+            this.label13.Location = new System.Drawing.Point(654, 634);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(71, 16);
             this.label13.TabIndex = 29;
@@ -207,9 +223,10 @@
             // 
             // btncolor2
             // 
-            this.btncolor2.Location = new System.Drawing.Point(586, 413);
+            this.btncolor2.Location = new System.Drawing.Point(783, 622);
+            this.btncolor2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btncolor2.Name = "btncolor2";
-            this.btncolor2.Size = new System.Drawing.Size(40, 23);
+            this.btncolor2.Size = new System.Drawing.Size(60, 37);
             this.btncolor2.TabIndex = 27;
             this.btncolor2.UseVisualStyleBackColor = true;
             // 
@@ -217,7 +234,8 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(320, 420);
+            this.label11.Location = new System.Drawing.Point(313, 634);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(43, 16);
             this.label11.TabIndex = 28;
@@ -235,88 +253,100 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Enabled = false;
-            this.panel1.Location = new System.Drawing.Point(574, 149);
+            this.panel1.Location = new System.Drawing.Point(241, 669);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(268, 150);
+            this.panel1.Size = new System.Drawing.Size(727, 57);
             this.panel1.TabIndex = 30;
             // 
             // txtReservadas
             // 
-            this.txtReservadas.Location = new System.Drawing.Point(84, 105);
+            this.txtReservadas.Location = new System.Drawing.Point(670, 13);
+            this.txtReservadas.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtReservadas.Name = "txtReservadas";
-            this.txtReservadas.Size = new System.Drawing.Size(100, 20);
+            this.txtReservadas.Size = new System.Drawing.Size(45, 29);
             this.txtReservadas.TabIndex = 7;
             // 
             // txtlibres
             // 
-            this.txtlibres.Location = new System.Drawing.Point(85, 41);
+            this.txtlibres.Location = new System.Drawing.Point(256, 16);
+            this.txtlibres.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtlibres.Name = "txtlibres";
-            this.txtlibres.Size = new System.Drawing.Size(100, 20);
+            this.txtlibres.Size = new System.Drawing.Size(43, 29);
             this.txtlibres.TabIndex = 6;
             // 
             // txtOcupadas
             // 
-            this.txtOcupadas.Location = new System.Drawing.Point(84, 71);
+            this.txtOcupadas.Location = new System.Drawing.Point(436, 16);
+            this.txtOcupadas.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtOcupadas.Name = "txtOcupadas";
-            this.txtOcupadas.Size = new System.Drawing.Size(100, 20);
+            this.txtOcupadas.Size = new System.Drawing.Size(45, 29);
             this.txtOcupadas.TabIndex = 5;
             // 
             // txtTotalMesas
             // 
-            this.txtTotalMesas.Location = new System.Drawing.Point(84, 10);
+            this.txtTotalMesas.Location = new System.Drawing.Point(105, 16);
+            this.txtTotalMesas.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtTotalMesas.Name = "txtTotalMesas";
-            this.txtTotalMesas.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalMesas.Size = new System.Drawing.Size(45, 29);
             this.txtTotalMesas.TabIndex = 4;
             // 
             // lab
             // 
             this.lab.AutoSize = true;
-            this.lab.Location = new System.Drawing.Point(12, 44);
+            this.lab.Location = new System.Drawing.Point(196, 19);
+            this.lab.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lab.Name = "lab";
-            this.lab.Size = new System.Drawing.Size(31, 13);
+            this.lab.Size = new System.Drawing.Size(52, 21);
             this.lab.TabIndex = 3;
-            this.lab.Text = "libres";
+            this.lab.Text = "Libres";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Location = new System.Drawing.Point(336, 19);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.Size = new System.Drawing.Size(79, 21);
             this.label3.TabIndex = 2;
             this.label3.Text = "Ocupadas";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 108);
+            this.label2.Location = new System.Drawing.Point(558, 19);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.Size = new System.Drawing.Size(89, 21);
             this.label2.TabIndex = 1;
             this.label2.Text = "Reservadas";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Location = new System.Drawing.Point(7, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.Size = new System.Drawing.Size(90, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Total Mesas";
             // 
             // Formulario_Ventas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(893, 445);
+            this.ClientSize = new System.Drawing.Size(1340, 766);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.btncolor2);
             this.Controls.Add(this.btncolor1);
             this.Controls.Add(this.panel3);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Formulario_Ventas";
             this.Text = "Formulario_Ventas";
+            this.Load += new System.EventHandler(this.mesas_Load);
             this.panel3.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
